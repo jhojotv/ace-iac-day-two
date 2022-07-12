59193b7e9c4e714453d8cae48141df9ac4fd2aca
jhojotv <108984907+jhojotv@users.noreply.github.com>
2022-07-12 10:39:23 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-     organization = "jhojo-netops"
+    organization = "jhojo-netops"
     workspaces {
       name = "ace-iac-day-two"
     }
